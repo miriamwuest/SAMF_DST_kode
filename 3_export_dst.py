63d6b1e47d6a9d53a045ad0a1ad3ba280d8206c5
miriamwuest <miriam.w@econ.ku.dk>
2026-08-11 15:03:15 +0200

    small add on: pivot tables

--- 3_export_dst.py
+++ 3_export_dst.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from dstapi import DstApi
 
 # for all yearly tables, we set the yearUntil variable to make updates easy.
@@ -15,6 +17,16 @@ def export_data(package):
 
 
 # add a package for each table you want to export data from, with the table ID, output file name, and variable values to filter the data by.
+# HISB3: Befolkning
+
+pkgHISB3 = {
+    "table_id": "HISB3",
+    "output_file": "data/HISB3_data.csv",
+    "variable_values": [
+        ["M+K", "LFT", "DT", "IND", "UDV"],  #  befolk hele landet osv
+        [f">=2001<={yearUntil}"],  # fra og med 2001 til og med {yearUntil}
+    ],
+}
 
 # INDPK107
 pkgIndk = {
@@ -30,19 +42,18 @@ pkgIndk = {
     ],
 }
 
-# FOLK1A
-pkgBefolkning = {
-    "table_id": "FOLK1A",
-    "output_file": "data/befolkning_data.csv",
-    "variable_values": [
-        ["000"],  # hele landet
-        ["TOT"],  # m og k i alt
-        ["*"],  # alder
-        ["TOT"],  # i alt
-        [">=2008K1<=2026K2"],  # fra og med 2008K1 til og med 2026K2
-    ],
-}
 
 # use the export_data function to export data from each table defined in the packages.
+export_data(pkgHISB3)
 export_data(pkgIndk)
-export_data(pkgBefolkning)
+
+
+# pivot tables may be neccessary to make the data easier to work with. The following code pivots the HISB3 data and saves it to a new CSV file.
+
+# hisb
+HISB3_data = pd.read_csv("data/HISB3_data.csv", sep=",", encoding="utf-8")
+# print(HISB3_data)
+
+pivoted = HISB3_data.pivot(index="BEVÆGELSE", columns="TID", values="INDHOLD")
+pivoted.to_csv("data/HISB3_data_pivoted.csv", sep=",", encoding="utf-8")
+# print(pivoted)
